e7916e249fb02b07f3afa2b280f7678a1a258e29
xiaolong <wuxingtianzhe@buaa.edu.cn>
2019-06-27 15:14:27 +0800

    添加了protobuf序列化信息

--- Game/GameMsg.h
+++ Game/GameMsg.h
@@ -1,10 +1,28 @@
 ﻿#pragma once
 #include "zinx.h"
+#include<google/protobuf/message.h>
 class GameMsg :
 	public UserData
 {
 public:
-	GameMsg();
+	/*用户请求的信息*/
+	google::protobuf::Message* pMsg = NULL;
+	enum MSG_TYPE
+	{
+		MSG_TYPE_LOGIN_ID_NAME = 1,
+		MSG_TYPE_CHAT_CONTENT = 2,
+		MSG_TYPE_NEW_POSITION = 3,
+		MSG_TYPE_BROADCAST = 200,
+		MSG_TYPE_LOGOFF_ID_NAME=201,
+		MSG_TYPE_SRD_POSITION=202
+	}enMsgType;
+	/* 已知消息内容创建消息对象*/
+	GameMsg(MSG_TYPE _type, google::protobuf::Message* _pMsg);
+	/*将字节流内容转换成消息结构*/
+	GameMsg(MSG_TYPE _type, std::string _stream);
+	/*序列化本消息*/
+	std::string serialize();	
+	
 	virtual ~GameMsg();
 };
 
--- Game/main.cpp
+++ Game/main.cpp
@@ -1,7 +1,28 @@
 ﻿#include"GameChannel.h"
+
+#include"msg.pb.h"
+#include"GameMsg.h"
 int main(int argc, char *argv[])
 {
+	pb::SyncPid *pmsg = new pb::SyncPid();
+	pmsg->set_pid(1);
+	pmsg->set_username("test");
 
+	GameMsg gm(GameMsg::MSG_TYPE_LOGIN_ID_NAME, pmsg);
+	auto output = gm.serialize();
+
+	for (auto byte : output)
+	{
+		printf("%02X ", byte);
+	}
+	puts("");
+
+	char buff[] = { 0x08, 0x01, 0x12, 0x04 ,0x74, 0x65, 0x73, 0x74 };
+	std::string input(buff, sizeof(buff));
+
+	auto ingm = GameMsg(GameMsg::MSG_TYPE_LOGIN_ID_NAME, input);
+	std::cout << dynamic_cast<pb::SyncPid *> (ingm.pMsg)->pid() << std::endl;
+	std::cout << dynamic_cast<pb::SyncPid *> (ingm.pMsg)->username() << std::endl;
 	ZinxKernel::ZinxKernelInit();
 	/*add the listen channle*/
 	ZinxKernel::Zinx_Add_Channel(*(new ZinxTCPListen(8899 , new GameConnFact())));
